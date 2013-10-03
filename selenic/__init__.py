bc1a3519e1d1bbcbf6a25e3c10c5ee40367d889b
Louis-Dominique Dubeau <ldd@lddubeau.com>
2013-10-03 14:03:53 -0400

    Patch ActionChains when running 2.35.0.

--- selenic/__init__.py
+++ selenic/__init__.py
@@ -1,1 +1,18 @@
+from selenium.webdriver.common.action_chains import ActionChains
+import selenium
+
 from config import *
+
+if selenium.__version__ == "2.35.0":
+    # Work around bug in 2.35.0
+    def send_keys(self, *keys_to_send):
+        """
+        Sends keys to current focused element.
+
+        :Args:
+        - keys_to_send: The keys to send.
+        """
+        self.key_down(keys_to_send)
+        return self
+
+    ActionChains.send_keys = send_keys
