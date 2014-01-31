8e7efd67a82230aca99c5393f9394dea05615a7c
Louis-Dominique Dubeau <ldd@lddubeau.com>
2014-01-30 19:59:19 -0500

    New version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="selenic",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     author="Louis-Dominique Dubeau",
     author_email="ldd@lddubeau.com",
