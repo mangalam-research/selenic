5f41a6c748acee486cf157b7373c8d4c1efa2e4d
Louis-Dominique Dubeau <ldd@lddubeau.com>
2014-01-30 19:58:43 -0500

    Update for newer selenium version.

--- selenic/__init__.py
+++ selenic/__init__.py
@@ -3,17 +3,18 @@ from distutils.version import StrictVersion
 from selenium.webdriver.common.action_chains import ActionChains
 import selenium
 
+# To rexport here.
 from .config import *
 
 sel_ver = StrictVersion(selenium.__version__)
-v2_37_2 = StrictVersion("2.37.2")
+v2_35_0 = StrictVersion("2.35.0")
 
-if sel_ver > v2_37_2:
+if sel_ver < v2_35_0:
     raise Exception("please ascertain whether the ActionChains.send_keys "
                     "patch is required for Selenium version: " +
                     selenium.__version__)
 
-if sel_ver >= StrictVersion("2.35.0") and sel_ver <= v2_37_2:
+if sel_ver >= v2_35_0 and sel_ver <= StrictVersion("2.37.2"):
     # Work around bug
     def send_keys(self, *keys_to_send):
         """
