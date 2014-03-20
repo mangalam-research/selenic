571d89c9e2eb5db6cada58b8e23626b3ab9f887d
Louis-Dominique Dubeau <ldd@lddubeau.com>
2014-03-20 16:18:35 -0400

    Version bump.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="selenic",
-    version="0.1.2",
+    version="0.2.0",
     packages=find_packages(),
     author="Louis-Dominique Dubeau",
     author_email="ldd@lddubeau.com",
@@ -10,7 +10,7 @@ setup(
     license="MPL 2.0",
     keywords=["selenium", "testing"],
     url="https://github.com/mangalam-research/selenic",
-    #use_2to3=True,
+    # use_2to3=True,
     classifiers=[
         "Programming Language :: Python",
         #"Programming Language :: Python :: 3",
