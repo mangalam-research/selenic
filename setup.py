9276ca753cbfc376b235bb7edf1a0d13348cfcb1
Louis-Dominique Dubeau <ldd@lddubeau.com>
2014-04-17 11:24:03 -0400

    Version bump.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="selenic",
-    version="0.2.0",
+    version="0.3.0",
     packages=find_packages(),
     author="Louis-Dominique Dubeau",
     author_email="ldd@lddubeau.com",
